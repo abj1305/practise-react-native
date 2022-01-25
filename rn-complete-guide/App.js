0a30cad6c3f6f82e1abf3c4baa800a43a6691658
Abhijeet Sakhalkar <abhijeet.sakhalkar@wipro.com>
2022-01-25 19:39:01 +0530

    25-1-2021- office

--- rn-complete-guide/App.js
+++ rn-complete-guide/App.js
@@ -1,23 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View , TextInput, Button} from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
-  return ( 
-   <View style = {{padding: 50}}>
-     <View style = {{flexDirection: 'row', justifyContent: 'space-between', alignItems:'center'}}>
-        <TextInput 
-         placeholder="Course Goal" 
-         style = {{width: '80%', borderBottomColor: 'black', borderWidth: 1, padding: 10} }/>
-        <Button title = "ADD"/>
-     </View>
-     <View>
-        
-     </View>
-     <StatusBar style="auto" />
-   </View>
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    // console.log(enteredGoal);
+    // setCourseGoals([...courseGoals, enteredGoal]);
+    setCourseGoals((currentGoals) => [...currentGoals, enteredGoal]);
+  };
+
+  return (
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title="ADD" onPress={addGoalHandler} />
+      </View>
+      <View>
+        <Text>
+          {courseGoals.map((goal) => (
+            <Text key={goal}>{goal}</Text>
+          ))}
+        </Text>
+      </View>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  screen: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  input: {
+    width: "80%",
+    borderBottomColor: "black",
+    borderWidth: 1,
+    padding: 10,
+  },
 });
