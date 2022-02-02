8d92599ee22a8cda5a4cdf8b8aab805c4a951e4a
Abhijeet Sakhalkar <abhijeet.sakhalkar@wipro.com>
2022-02-02 12:51:04 +0530

    guess a number basic skeleton done

--- guess-a-number/App.js
+++ guess-a-number/App.js
@@ -10,6 +10,11 @@ export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [guessRounds, setGuessRounds] = useState(0);
 
+  const configureNewGameHandler = () => {
+    setGuessRounds(0);
+    setUserNumber(null);
+  };
+
   const startGameHandler = (selectedNumber) => {
     setUserNumber(selectedNumber);
     setGuessRounds(0);
@@ -25,7 +30,13 @@ export default function App() {
       <GameScreen userChoice={userNumber} onGameOver={setGuessRounds} />
     );
   } else if (guessRounds > 0) {
-    content = <GameOverScreen />;
+    content = (
+      <GameOverScreen
+        roundNumber={guessRounds}
+        userNumber={userNumber}
+        onRestart={configureNewGameHandler}
+      />
+    );
   }
   return (
     <View style={styles.screen}>
--- guess-a-number/screens/GameOverScreen.js
+++ guess-a-number/screens/GameOverScreen.js
@@ -1,10 +1,13 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 
 const GameOverScreen = (props) => {
   return (
     <View style={styles.screen}>
       <Text>The Game Is Over!</Text>
+      <Text>Number of Rounds: {props.roundNumber}</Text>
+      <Text>Number was: {props.userNumber}</Text>
+      <Button title="Start New Game" onPress={props.onRestart} />
     </View>
   );
 };
